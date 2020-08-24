b853469206ce53f1322c5d53fb07da10d8108482
Rasmus <rasmus@nolimitation.dk>
2020-08-24 18:59:11 +0200

    newst changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Dashboard from './components/dashboard/Dashboard';
+import Navbar from './components/layouts/Navbar';
+import Profile from './components/profile/Profile';
+import Queue from './components/queue/Queue';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/'component={Dashboard} />
+          <Route path='/profile'component={Profile} />
+          <Route path='/create'component={Queue} /> 
+        </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
