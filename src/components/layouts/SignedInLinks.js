2babd3157bf79de0989600e6276963a6725544f6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 17:26:25 +0100

    more testing

--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -6,7 +6,7 @@ const SignedInLinks = () => {
     <div>
       <ul className="right">
         <li><NavLink to='/'>Log Out</NavLink></li>
-        <li><NavLink to='/profile' className="btn btn-floating pink lighten-1">RP</NavLink></li>
+        <li><NavLink to='/profile' className="btn btn-floating blue darken-1">RP</NavLink></li>
       </ul>
     </div>
   )
