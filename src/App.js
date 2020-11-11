d5dc1156919d5dc6dc90c4b2f5854bff2102cab2
Rasmus <rasmus@nolimitation.dk>
2020-11-11 10:15:56 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-/* import Dashboard from './components/dashboard/Dashboard'; */
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layouts/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './components/auth/SignIn'
@@ -12,7 +12,7 @@ function App() {
       <div className="App blue-grey darken-1">
         <Navbar />
         <Switch>
-          {/* <Route exact path='/'component={Dashboard} /> */}
+          <Route exact path='/'component={Dashboard} />
           <Route path='/login'component={SignIn} />
           <Route path='/register'component={SignUp} />
           <Route path='/profile'component={Profile} />
