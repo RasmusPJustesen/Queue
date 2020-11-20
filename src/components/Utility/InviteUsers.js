566a9de1c458b1dc4d9553e8d08855d915e321cb
Nicklas Degnebolig <nicklasdegnebolig@gmail.com>
2020-11-20 12:07:40 +0100

    createroom and inviteuser mockup done

--- src/components/Utility/InviteUsers.js
+++ src/components/Utility/InviteUsers.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react'
 class InviteUsers extends Component {
     render() {
       return (
-        <div></div>
+        
+        <div className="user input-field col s12">
+        <input id="email" type="text" className="validate"/>
+        <label className="active" for="email">Email:</label>
+        <button className="btn deep-orange">Invite</button>
+          <div className="row mt-3">
+          <div className="col m3 card blue-grey darken-1">
+              <div className="card-content">
+                <img src="/images/nicklas.jpg" className="responsive-img" alt=""/>
+              </div>
+              <div className="card-action">
+                <p className="user-song-author">Nicklas Degnebolig</p>
+              </div>
+            </div>
+
+            <div className="col m3 card blue-grey darken-1">
+              <div className="card-content">
+                <img src="/images/rasmus.jpg" className="responsive-img" alt=""/>
+              </div>
+              <div className="card-action">
+                <p className="user-song-author">Rasmus Justessen</p>
+              </div>
+            </div>
+          </div>
+       </div>
+        
       )
     }
 }
--- src/components/dashboard/CreateRoom.js
+++ src/components/dashboard/CreateRoom.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import InviteUsers from '../Utility/InviteUsers';
 
 const CreateRoom = () => {
   return (
     <div className="container createroom mt-5">
       <div className="row">
-        <div className="col s6">
-          
+        <div className="col s6 mt-5">
+          <div className="input-field col s12">
+            <input id="room" type="text" className="validate"/>
+            <label className="active" for="room">Rum navn:</label>
+           </div>
+           <InviteUsers />
         </div>
         <div className="col s6">
          <button className="btn deep-orange mt-3 right"> <i class="fas fa-plus mr-3"></i> Opret</button>
