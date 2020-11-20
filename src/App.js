5db431212ddc5492af4cd690dfd53fc3c3f225cc
Nicklas Degnebolig <nicklasdegnebolig@gmail.com>
2020-11-20 11:13:29 +0100

    showing dashoard rooms - working on createroom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Dashboard from './components/layouts/Dashboard';
 import Navbar from './components/layouts/Header';
+import CreateRoom from './components/dashboard/CreateRoom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/'component={Dashboard} />
+          <Route exact path='/createroom'component={CreateRoom} />
           <Route path='/login'component={SignIn} />
           <Route path='/register'component={SignUp} />
         </Switch>
--- src/components/dashboard/AllRooms.js
+++ src/components/dashboard/AllRooms.js
@@ -2,23 +2,29 @@ import React from 'react'
 
 const AllRooms = () => {
   return (
-    <div className="card deep-orange accent-2">
-      <div className="card-content white-text">
-        <span className="card-title">Options</span>
-        <p>List of options</p>
-        <ul>
-            <li>Option</li>
-            <li>Option</li>
-            <li>Option</li>
-            <li>Option</li>
-            <li>Option</li>
-        </ul>
-      </div>
-      <div className="card-action">
-        <button className="btn deep-orange accent-4">Action1</button>
-        <button className="btn deep-orange accent-4 ml-1">Action2</button> 
-      </div>
+    <div className="singleroom card col s3 deep-orange ml-4">
+    <div className="card-content row">
+        <div className="top col s12">
+             <i class="far fa-star left"></i>
+             <i class="fas fa-trash right"></i>
+        </div>
+        <div className="middle col s12 valign-wrapper center-align">
+            <h5>Queify Playliste</h5>
+            <p className="small-text"> <i class="fas fa-calendar-day mr-2"></i>  20. November</p>
+        </div>
+        
+        <div className="bottom col s12 mt-4 row">
+            <div className="infomation col s6">
+                <p className="small-text">Users</p>
+                <p className="big">12</p>
+            </div>
+            <div className="infomation col s6">
+                <p className="small-text">Songs</p>
+                <p className="big">58</p>
+            </div>
+        </div>
     </div>
+</div>
   ) 
 }
 
