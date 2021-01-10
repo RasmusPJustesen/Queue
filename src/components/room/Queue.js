a83ae1238bb27499cb264e38788ec805f89059bf
BuildTools <unconfigured@null.spigotmc.org>
2021-01-10 11:05:10 +0100

    fixed queue

--- src/components/room/Queue.js
+++ src/components/room/Queue.js
@@ -6,29 +6,33 @@ const Queue = ({queue}) => {
     const htmlList = queue ? (
         Object.keys(queue).map((item, i, arr) => {
             console.log(queue, item);
-            return (
-            <div className="col m2 card blue-grey darken-1" key={item}>
-                <div className="card-content">
-                  
-                </div>
-                <div className="card-action">
-              
-                <iframe
-                        style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "20vh"
-                        }}
-                        title={item.title}
-                        src={`https://www.youtube.com/embed/${item}`}
-                        frameBorder="0"
-                    />
-                </div>
+            
+            if(i <= 4) {
+                return (
+                    <div className="card blue-grey darken-1" key={item}>
+                        <div className="card-content"></div>
+                        <div className="card-action">
 
-            </div>
-            )
+                            <iframe
+                                style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                width: "100%",
+                                height: "20vh"
+                                }}
+                                title={item.title}
+                                src={`https://www.youtube.com/embed/${item}`}
+                                frameBorder="0"
+                            />
+
+                        </div>
+                    </div>
+                )
+            } else {
+                return null;
+            }
+            
         })
     ) : (
         <div className="center">no posts yet</div>
@@ -37,9 +41,8 @@ const Queue = ({queue}) => {
     return (
         <div className="Queue">
         <h5 className="mt-0">Næste i køen</h5>
-            <div className="row">
+            <div className="grid">
                 {htmlList}
-                
             </div>
         </div>  
     )
