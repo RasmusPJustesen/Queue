f3ded594ae9f966fd1e52cf108b245b1be9355de
Nicklas Hvid Degnebolig <nick7121@edu.easj.dk>
2021-01-25 15:37:47 +0100

    using react-youtube libary

--- src/components/room/CurrentlyPlaying.js
+++ src/components/room/CurrentlyPlaying.js
@@ -1,34 +1,26 @@
 import React from 'react'
-
+import YouTube from 'react-youtube';
 const CurrentlyPlaying = (props) => {
+
+    const opts = {
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+      
+     
 
     return (
         <div className="CurrentlyPlaying">
         <img src={props.videoID.image} className="current-song-img" alt=""/>
             <div className="row center-align">
-               
-                <iframe         id="video"
-                                
-                                style={{
-                                position: "absolute",
-                                display: 'none',
-                                top: 0,
-                                left: 0,
-                                width: "100%",
-                                height: "20vh"
-                                }}
-                                className="current-song-img"
-                                title="test"
-                                allow='autoplay'
-                                src={`https://www.youtube.com/embed/${props.videoID.id}?autoplay=1&enablejsapi=1`}
-                                frameBorder="0"
-                                
-                            />
-               
+            <YouTube videoId={props.videoID.id} opts={opts}  className="iframe" />    
             </div>
             <div className="row center-align mb-0">
                 <h6 className="m-0">Afspiller lige nu:</h6>
                 <h5 className="m-0">{props.videoID.title}</h5>
+               
             </div>
         </div>
     ) 
